cdd00f891fc152e56b84a0b50e79e7d2a85350fd
zh.sqy <zh.sqy@qq.com>
2016-03-17 16:40:12 +0800

    修改依赖，新增数据库操作

---
+++ generator-database/src/main/java/org/hsweb/generator/db/DatabaseFactory.java
@@ -0,0 +1,59 @@
+package org.hsweb.generator.db;
+
+
+import org.webbuilder.sql.DataBase;
+import org.webbuilder.sql.DataBaseMetaData;
+import org.webbuilder.sql.support.MysqlDataBaseMetaData;
+import org.webbuilder.sql.support.OracleDataBaseMetaData;
+import org.webbuilder.sql.support.common.CommonDataBase;
+
+import java.util.Properties;
+
+/**
+ * 数据库工厂，用于创建进行数据库操作的对象.
+ * 目前只支持mysql和oracle数据库
+ * Created by 浩 on 2016-03-17 0017.
+ */
+public class DatabaseFactory {
+
+    private DatabaseFactory() {
+    }
+
+    /**
+     * 根据配置创建一个mysql数据库操作对象=
+     *
+     * @param jdbcProperties JDBC配置文件<br/>
+     *                       配置文件应包含以下配置:
+     *                       <ul>
+     *                       <li>jdbc.driver.class</li>
+     *                       <li>jdbc.url</li>
+     *                       <li>jdbc.username</li>
+     *                       <li>jdbc.password</li>
+     *                       </ul>
+     * @return mysql数据库操作对象
+     */
+    public static DataBase createMysqlDatabase(Properties jdbcProperties) {
+        DataBaseMetaData metaData = new MysqlDataBaseMetaData();
+        DataBase dataBase = new CommonDataBase(metaData, new CommonJdbcSqlExecutor(jdbcProperties));
+        return dataBase;
+    }
+
+    /**
+     * 根据配置创建一个oracle数据库操作对象=
+     *
+     * @param jdbcProperties JDBC配置文件<br/>
+     *                       配置文件应包含以下配置:
+     *                       <ul>
+     *                       <li>jdbc.driver.class</li>
+     *                       <li>jdbc.url</li>
+     *                       <li>jdbc.username</li>
+     *                       <li>jdbc.password</li>
+     *                       </ul>
+     * @return oracle数据库操作对象
+     */
+    public static DataBase createOracleDatabase(Properties jdbcProperties) {
+        DataBaseMetaData metaData = new OracleDataBaseMetaData();
+        DataBase dataBase = new CommonDataBase(metaData, new CommonJdbcSqlExecutor(jdbcProperties));
+        return dataBase;
+    }
+}
---
+++ generator-database/src/main/java/org/hsweb/generator/db/template/TableTemplateOutput.java
@@ -0,0 +1,46 @@
+package org.hsweb.generator.db.template;
+
+import org.hsweb.generator.CodeTemplate;
+import org.hsweb.generator.template.TemplateOutput;
+import org.webbuilder.sql.DataBase;
+import org.webbuilder.sql.TableMetaData;
+
+import java.util.Map;
+
+/**
+ * 创建表的模板输出，将模板输出为数据库表。在模板输出配置中，需要提供 table参数，类型为{@link TableMetaData}<br>
+ * Created by 浩 on 2016-03-17 0017.
+ */
+public class TableTemplateOutput implements TemplateOutput {
+
+    protected CodeTemplate template;
+
+    protected DataBase dataBase;
+
+    public TableTemplateOutput(DataBase dataBase) {
+        this.dataBase = dataBase;
+    }
+
+    @Override
+    public void output() {
+        //生成数据库表
+        Map<String, Object> config = template.getInput().getConfig();
+        if (config != null) {
+            TableMetaData metaData = (TableMetaData) config.get("table");
+            if (metaData != null) {
+                try {
+                    dataBase.createTable(metaData);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else {
+            throw new RuntimeException("未获取到模板输入配置");
+        }
+    }
+
+    @Override
+    public void setTemplate(CodeTemplate template) {
+        this.template = template;
+    }
+}
