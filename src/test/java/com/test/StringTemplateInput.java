ea0d26801998ea815ea69773ffec8d0d1e37d213
zh.sqy <zh.sqy@qq.com>
2016-03-17 15:38:04 +0800

    修改编码为utf8

--- src/test/java/com/test/StringTemplateInput.java
+++ src/test/java/com/test/StringTemplateInput.java
@@ -2,15 +2,19 @@ package com.test;
 
 import org.hsweb.generator.template.TemplateInput;
 
-/**
- * Created by ºÆ on 2016-03-17 0017.
- */
+import java.util.Map;
+
 public class StringTemplateInput implements TemplateInput {
 
     public String template;
 
     public StringTemplateInput(String template) {
         this.template = template;
+    }
+
+    @Override
+    public Map<String, Object> getConfig() {
+        return null;
     }
 
     @Override
