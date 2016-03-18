812a3670a2fa2f949ec5729b172d71f8d7e271d7
zh.sqy <zh.sqy@qq.com>
2016-03-18 23:45:32 +0800

    新增日志配置，可以将logback的日志输出绑定到JTextArea

--- generator-boot/src/test/java/org/hsweb/generator/test/SwingTests.java
+++ generator-boot/src/test/java/org/hsweb/generator/test/SwingTests.java
@@ -2,14 +2,6 @@ package org.hsweb.generator.test;
 
 import org.hsweb.generator.boot.GeneratorApplication;
 import org.hsweb.generator.swing.SwingGeneratorApplication;
-import org.junit.Test;
-
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 /**
  * Created by 浩 on 2016-03-18 0018.
